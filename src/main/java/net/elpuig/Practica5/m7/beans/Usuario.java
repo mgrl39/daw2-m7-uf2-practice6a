4e3b865394ab387961624d51aad97a4f4871e356
isard <mgrl39@ubuntu-22.04>
2025-02-09 00:16:13 +0100

    class Usuario created

---
+++ src/main/java/net/elpuig/Practica5/m7/beans/Usuario.java
@@ -0,0 +1,38 @@
+package net.elpuig.Practica5.m7.beans;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSessionBindingEvent;
+import jakarta.servlet.http.HttpSessionBindingListener;
+
+public class Usuario implements HttpSessionBindingListener {
+
+    private String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void valueBound(HttpSessionBindingEvent event) {
+        System.out.println("Usuario añadido a la sesión. ID: " + event.getSession().getId());
+        ServletContext context = event.getSession().getServletContext();
+        synchronized (context) {
+            Integer usuariosValidados = (Integer) context.getAttribute("usuariosValidados");
+            context.setAttribute("usuariosValidados", (usuariosValidados == null) ? 1 : usuariosValidados++);
+        }
+    }
+
+    @Override
+    public void valueUnbound(HttpSessionBindingEvent event) {
+        System.out.println("Usuario eliminado de la sesión. ID: " + event.getSession().getId());
+        ServletContext context = event.getSession().getServletContext();
+        synchronized (context) {
+            Integer usuariosValidados = (Integer) context.getAttribute("usuariosValidados");
+            context.setAttribute("usuariosValidados", (usuariosValidados == null || usuariosValidados <= 0) ? 0 : usuariosValidados - 1);
+        }
+    }
+}
